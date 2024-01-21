76dd891fb8643d8760d2b129bcca280252b5240b
Tal Alfi <46304735+alfital2@users.noreply.github.com>
2024-01-21 19:38:58 +0200

    fixing current_position naming

--- node.py
+++ node.py
@@ -3,9 +3,9 @@ class Node:
         self.grid = grid
         self.parent = parent
         self.cost = cost
-        self.current_pos = current_pos
+        self.current_position = current_pos
 
-   def make_node(self,target):
+    def make_node(self,target):
        move_cost = self.grid.get_cost(self.current_position, target)
        new_grid = self.grid.make_cloned_move(self.current_position, target)
        return Node(grid=new_grid,parent=self,cost = self.cost+ move_cost,current_pos=target)
